0f9b07e6cd3958c35ce409ed5735a79964153c5e
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-08-15 20:13:24 -0400

    fix typos

--- src/components/About.js
+++ src/components/About.js
@@ -32,26 +32,26 @@ function About() {
         <div>
           <p>
             ~2020~<br/>
-            Having graduated from school into the COVID19 pandemic, I struggled to puruse my dream of becoming a fulltime performing musician. 
-            Instead, I chose to puruse a career as a freelance music educator. I have always loved children and working in schools.
-            Here, I developed the strong communication skills and personal responsibility skills necessary for success running a self-employed buisness. 
+            Having graduated from school into the COVID19 pandemic, I struggled to pursue my dream of becoming a fulltime performing musician. 
+            Instead, I chose to pursue a career as a freelance music educator. I have always loved children and working in schools.
+            Here, I developed the strong communication skills and personal responsibility skills necessary for success running a self-employed business. 
             Being an educator also taught me to balance my instincts toward perfection with time 
             constraints and to be extremely efficient. 
           </p>
           <p>
             ~2021 to 2023~<br/>
             I began to practice programming techniques and languages in the summer of 2021. I spent my early months completing certification and career courses
-            offerred by CodeAcademy. I was enjoying honing my skill, but it was difficult to validate a career transition. I had a successful and admittedly overflowing private lesson studio
-            of low brass students in the greater Housto area and was expecting the birth of my first child. As 2023 approached, I set a New Year's Resolution to officially begin my career transition.
+            offered by CodeAcademy. I was enjoying honing my skill, but it was difficult to validate a career transition. I had a successful and admittedly overflowing private lesson studio
+            of low brass students in the greater Houston area and was expecting the birth of my first child. As 2023 approached, I set a New Year's Resolution to officially begin my career transition.
             I applied for and was accepted to the Flatiron School in NYC for to study software engineering. 
-            After completing over 500 hours of coding, undergoing five seperate project reviews conducted by experienced instructors, and passing four interview-like coding challenges, I successfully graduated from my program.
+            After completing over 500 hours of coding, undergoing five separate project reviews conducted by experienced instructors, and passing four interview-like coding challenges, I successfully graduated from my program.
           </p>
           <p>
             ~Now~<br/>
             When I'm not programming, you'll find me spending time with my wife, Chloe, our toddler, Hazel, and our two dogs, Wyatt and Bella.
             Together, we enjoy visiting new playgrounds, dogparks, going to museums, and attending live-performances around the city.
             I have also been spending my reading more, and I am thrilled to share that I have recently joined a local book club!
-            My wife and I both still teach music lessons and enjoy getting to perform live music when we have time. Overal, we are having a blast being back in NYC!
+            My wife and I both still teach music lessons and enjoy getting to perform live music when we have time. Overall, we are having a blast being back in NYC!
           </p>
           </div> : null}
         <div className="btn-wrapper">
